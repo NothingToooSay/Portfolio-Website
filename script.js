393eba84f12bc65282fd8b45e34b5cee31a248f8
NothingToooSay <ilya.samoha88@gmail.com>
2024-04-05 11:02:57 +0200

    portfolio-web

---
+++ script.js
@@ -0,0 +1,50 @@
+const sr = ScrollReveal ({
+    distance: '35px',
+    duration: 2400,
+    reset: true,
+});
+
+sr.reveal('.logo', {
+    delay: 210,
+    origin: 'bottom',
+});
+
+sr.reveal('.navlist', {
+    delay: 310,
+    origin: 'top',
+});
+
+sr.reveal('.top-btn', {
+    delay: 410,
+    origin: 'right',
+});
+
+sr.reveal('.main-content h4', {
+    delay: 510,
+    origin: 'top',
+});
+
+sr.reveal('.main-content h1', {
+    delay: 610,
+    origin: 'left',
+});
+
+sr.reveal('.main-content p', {
+    delay: 710,
+    origin: 'right',
+});
+
+sr.reveal('.social', {
+    delay: 610,
+    origin: 'top',
+});
+
+sr.reveal('.main-btn', {
+    delay: 810,
+    origin: 'right',
+});
+
+sr.reveal('.bottom', {
+    delay: 1010,
+    origin: 'left',
+});
